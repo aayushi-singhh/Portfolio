d2866c3e5cf28d5b08fdff1569fdb335460ac831
Aayushi Singh <aayushisingh239@gmail.com>
2025-07-12 00:05:25 +0530

    added the images

--- portfolio.js
+++ portfolio.js
@@ -169,3 +169,66 @@
         document.querySelectorAll('.course-card').forEach(card => {
             courseObserver.observe(card);
         });
+
+        //cursor image animation
+        const images=[
+            "https://wallpapers.com/images/hd/netflix-profile-pictures-5yup5hd2i60x7ew3.jpg",
+            "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp",
+            "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.webp",
+            "https://i.pinimg.com/1200x/c3/cc/31/c3cc317a85e0953ea7320d4792353374.jpg"
+        ]
+        const container=document.getElementById("container");
+
+        let currentIndex=0;
+        let lastX=0;
+        let lastY=0;
+        let distanceThreshold= window.innerWidth <900? 100: 180;
+
+        window.addEventListener("resize", () => {
+            distanceThreshold = window.innerWidth < 900 ? 100 : 180;
+        });
+
+        window.addEventListener("mousemove", (e) => {
+            const dx = e.clientX - lastX;
+            const dy = e.clientY - lastY;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance > distanceThreshold) {
+                createTrail(e.clientX, e.clientY);
+                lastX = e.clientX;
+                lastY = e.clientY;
+            }
+        });
+
+        function createTrail(x,y){
+            const img=document.createElement("img");
+            img.classList.add("image-trail");
+            img.src=images[currentIndex];
+            container.appendChild(img);
+
+            currentIndex=(currentIndex+1)%images.length;
+            gsap.set(img,{
+                x:x,
+                y:y,
+                scale:0,
+                opacity:0,
+                rotation:gsap.utils.random(-20,20)
+            });
+            gsap.to(img,{
+                scale:1,
+                opacity:1,
+                duration:0.4,
+                ease:"power2.out"
+            });
+            gsap.to(img,{
+                scale:0.2,
+                opacity:0,
+                duration:1,
+                delay:0.3,
+                ease:"power2.in",
+                onComplete: () => {
+                    img.remove();
+                },
+            });
+        }
+        
